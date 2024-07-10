4b76c4820dc4bff3521d660775b162e46aa77205
Timothy Roff <tjr214@mailbox.org>
2024-07-10 06:55:47 -0400

    Update to monorepo style management

--- cool-deploy.sh
+++ cool-deploy.sh
@@ -3,13 +3,14 @@
 # Source our variables from the generated env file
 source .env.coolify
 
-# Project path variables
+# Project path variable
 WASP_PROJECT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-MAIN_PROJECT_DIR=$(dirname "$WASP_PROJECT_DIR")
+PARENT_DIR=$(dirname "$WASP_PROJECT_DIR")
 
-# Deplot directories
-CLIENT_DEPLOY_DIR=$WASP_PROJECT_DIR/deploy/cool_client
-SERVER_DEPLOY_DIR=$WASP_PROJECT_DIR/deploy/cool_server
+# Deploy directories
+DEPLOY_DIR=$WASP_PROJECT_DIR/deploy
+CLIENT_DEPLOY_DIR=$DEPLOY_DIR/client
+SERVER_DEPLOY_DIR=$DEPLOY_DIR/server
 
 # Tell the client frontend where to find the server backend
 REACT_APP_API_URL=$WASP_SERVER_URL
@@ -32,49 +33,51 @@ fi
 echo -e "\033[1;36m🤖 --- PROJECT INFO...\033[0m"
 echo
 
-echo -e "\033[1;33mMain Project Dir:\033[0m $MAIN_PROJECT_DIR"
 echo -e "\033[1;33mWasp Project Dir:\033[0m $WASP_PROJECT_DIR"
-echo -e "\033[1;33mClient Location:\033[0m $WASP_WEB_CLIENT_URL"
-echo -e "\033[1;33mServer Location:\033[0m $REACT_APP_API_URL"
+echo -e "\033[1;33mClient Directory:\033[0m $CLIENT_DEPLOY_DIR"
+echo -e "\033[1;33mServer Directory:\033[0m $SERVER_DEPLOY_DIR"
+echo -e "\033[1;33mClient URL:\033[0m $WASP_WEB_CLIENT_URL"
+echo -e "\033[1;33mServer URL:\033[0m $REACT_APP_API_URL"
 echo
 
 # Get back to the `project` directory that contains the wasp dir and the git deploy dirs
-cd $MAIN_PROJECT_DIR
+cd $WASP_PROJECT_DIR
 
-if [ ! -d "client_build" ]; then
-  echo -e "\033[31m💀 --- Error: client_build/ directory does not exist! Please run setup script again. ---\033[0m"
+if [ ! -d "$CLIENT_DEPLOY_DIR" ]; then
+  echo -e "\033[31m💀 --- Error: \`$CLIENT_DEPLOY_DIR\` does not exist! Please run setup script again. ---\033[0m"
   echo
   exit 1
 fi
 
-if [ ! -d "server_build" ]; then
-  echo -e "\033[31m💀 --- Error: server_build/ directory does not exist! Please run setup script again. ---\033[0m"
+if [ ! -d "$SERVER_DEPLOY_DIR" ]; then
+  echo -e "\033[31m💀 --- Error: \`$SERVER_DEPLOY_DIR\` does not exist! Please run setup script again. ---\033[0m"
   echo
   exit 1
 fi
 
 echo -e "\033[1;31m❗️ --- CLEANING OUT OLD BUILDS...\033[0m"
 echo
-rm -rf server_build/db
-rm -rf server_build/sdk
-rm -rf server_build/server
-rm -rf server_build/src
-rm -rf server_build/package.json
-rm -rf server_build/package-lock.json
-rm -rf server_build/installedNpmDepsLog.json
-rm -rf server_build/Dockerfile
-rm -rf server_build/.waspinfo
-rm -rf server_build/.waspchecksums
-rm -rf server_build/.dockerignore
-echo -e "\033[33m✅ --- Sanitized server_build/ directory. ---\033[0m"
+cd $WASP_PROJECT_DIR
+rm -rf $SERVER_DEPLOY_DIR/db
+rm -rf $SERVER_DEPLOY_DIR/sdk
+rm -rf $SERVER_DEPLOY_DIR/server
+rm -rf $SERVER_DEPLOY_DIR/src
+rm -rf $SERVER_DEPLOY_DIR/package.json
+rm -rf $SERVER_DEPLOY_DIR/package-lock.json
+rm -rf $SERVER_DEPLOY_DIR/installedNpmDepsLog.json
+rm -rf $SERVER_DEPLOY_DIR/Dockerfile
+rm -rf $SERVER_DEPLOY_DIR/.waspinfo
+rm -rf $SERVER_DEPLOY_DIR/.waspchecksums
+rm -rf $SERVER_DEPLOY_DIR/.dockerignore
+echo -e "\033[33m✅ --- Sanitized \`$SERVER_DEPLOY_DIR\`. ---\033[0m"
 echo
-rm -rf client_build/assets
-rm -rf client_build/.gitkeep
-rm -rf client_build/favicon.ico
-rm -rf client_build/index.html
-rm -rf client_build/*.html
-rm -rf client_build/manifest.json
-echo -e "\033[33m✅ --- Sanitized client_build/ directory. ---\033[0m"
+rm -rf $CLIENT_DEPLOY_DIR/assets
+rm -rf $CLIENT_DEPLOY_DIR/.gitkeep
+rm -rf $CLIENT_DEPLOY_DIR/favicon.ico
+rm -rf $CLIENT_DEPLOY_DIR/index.html
+rm -rf $CLIENT_DEPLOY_DIR/*.html
+rm -rf $CLIENT_DEPLOY_DIR/manifest.json
+echo -e "\033[33m✅ --- Sanitized \`$CLIENT_DEPLOY_DIR\`. ---\033[0m"
 echo
 
 cd $WASP_PROJECT_DIR
@@ -104,15 +107,15 @@ if ! (npm install && REACT_APP_API_URL=$REACT_APP_API_URL npm run build); then
 fi
 
 echo
-echo -e "\033[1;32m🤖 --- SHIPPING INTO DIST FOLDERS...\033[0m"
+echo -e "\033[1;32m🤖 --- SHIPPING INTO DEPLOY DIRECTORIES...\033[0m"
 echo
 cd $WASP_PROJECT_DIR
-mv -f .wasp/build/web-app/build/* ../client_build
-echo -e "\033[33m✅ --- Moved static site files into client_build/ directory. ---\033[0m"
+mv -f .wasp/build/web-app/build/* $CLIENT_DEPLOY_DIR
+echo -e "\033[33m✅ --- Moved static site files into \`$CLIENT_DEPLOY_DIR\`. ---\033[0m"
 echo
-mv -f .wasp/build/* ../server_build
-rm -rf ../server_build/web-app/
-echo -e "\033[33m✅ --- Moved server code & sdk and Dockerfile into server_build/ directory. ---\033[0m"
+mv -f .wasp/build/* $SERVER_DEPLOY_DIR
+rm -rf $SERVER_DEPLOY_DIR/web-app/
+echo -e "\033[33m✅ --- Moved server code & sdk and Dockerfile into \`$SERVER_DEPLOY_DIR\`. ---\033[0m"
 echo
 
 echo
@@ -129,32 +132,17 @@ else
     COMMIT_MSG="Commit [$TIMESTAMP]"
 fi
 
-cd $MAIN_PROJECT_DIR
-cd client_build
-git add .
-git commit -m "Auto-Deploy: $COMMIT_MSG"
-if git push; then
-  echo
-  echo -e "\033[33m✅ --- Successfully pushed Client to GitHub. ---\033[0m"
-else
-  echo
-  echo -e "\033[1;31m🛑 --- Failed to push Client to GitHub! ---\033[0m"
-fi
-echo
-
-cd $MAIN_PROJECT_DIR
-cd server_build
-git add .
-git commit -m "Auto-Deploy: $COMMIT_MSG"
-if git push; then
-  echo
-  echo -e "\033[33m✅ --- Successfully pushed Server to GitHub. ---\033[0m"
-else
-  echo
-  echo -e "\033[1;31m🛑 --- Failed to push Server to GitHub! ---\033[0m"
-fi
-echo
 cd $WASP_PROJECT_DIR
+git add .
+git commit -m "Auto-Deploy: $COMMIT_MSG"
+if git push; then
+  echo
+  echo -e "\033[33m✅ --- Successfully pushed Everything(tm) to GitHub. ---\033[0m"
+else
+  echo
+  echo -e "\033[1;31m🛑 --- Failed to push Anything to GitHub! ---\033[0m"
+fi
+echo
 
 echo -e "Your App is available at: \033[1;34m$WASP_WEB_CLIENT_URL\033[0m"
 echo
