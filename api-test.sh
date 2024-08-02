cd2f067543a0065091f3b0441c072b5715ba4e52
Timothy Roff <tjr214@mailbox.org>
2024-08-02 06:53:58 -0400

    tjr214/coolify: Remove leaked API key
    Oh! The idiot I was!
    I Do Not Know How BUT: I let my servers URL and full-access API Token be put into the Repo!!! My mistake has been corrected and that API Key has been deleted. OK lets get back to work now. Silly me lol
    
    PEACE!

--- api-test.sh
+++ api-test.sh
@@ -2,10 +2,14 @@
 
 source .env.coolify
 
-COOLIFY_API_KEY="1|CWjPJnXad9NoWZogwjhebgvBEMsxkRhm0fUKAQCz9b727414"
-COOLIFY_BASE_URL="https://timlify.thetjr.com"
+# COOLIFY_API_KEY=""
+# COOLIFY_BASE_URL=""
 BEARER="Authorization: Bearer $COOLIFY_API_KEY"
 
+# ------------------------------------------------------------------------------
+# Parsing the JSON from Coolify's API requires the `jq` command line tool.
+# Make sure it is installed and available on the system.
+# ------------------------------------------------------------------------------
 detect_jq() {
   if ! command -v jq &> /dev/null; then
     echo -e "\033[1;31mERROR:\033[0m \033[31m\`jq\` is not installed. Please install it using the following commands:\033[0m"
@@ -27,17 +31,9 @@ detect_jq() {
   fi
 }
 
-detect_jq
-
-# local colors=(black red green yellow blue magenta cyan white reset)
-# local color_codes=(30 31 32 33 34 35 36 37 0)
-
-# local styles=(bold italic underline strikethrough)
-# local style_codes=(1 3 4 9)
-
-# local backgrounds=(black red green yellow blue magenta cyan white)
-# local background_codes=(40 41 42 43 44 45 46 47)
-
+# ------------------------------------------------------------------------------
+# GET_COOLIFY_VERSION
+# ------------------------------------------------------------------------------
 get_coolify_version() {
   local version=$(curl -s --request GET \
     --url $COOLIFY_BASE_URL/api/v1/version \
@@ -45,7 +41,11 @@ get_coolify_version() {
 echo "$version"
 }
 
-list_coolify_servers() {
+# ------------------------------------------------------------------------------
+# GET_COOLIFY_SERVERS
+# Optional: $1 = if already retrieved UUID, check to make sure it is valid.
+# ------------------------------------------------------------------------------
+get_coolify_servers() {
   if [ ! -z "$1" ]; then
     valid_server=0
   fi
@@ -78,22 +78,109 @@ list_coolify_servers() {
   return 0
 }
 
+# ------------------------------------------------------------------------------
+# LIST_COOLIFY_PROJECTS
+# ------------------------------------------------------------------------------
+list_coolify_projects() {
+  local projects=$(curl -s --request GET \
+    --url $COOLIFY_BASE_URL/api/v1/projects \
+    --header "$BEARER")
+  local project_count=$(jq '. | length' <<< "$projects")
+  for ((i = 0; i < project_count; i++)); do # Loop through all projects
+    local id=$(jq -r ".[$i].id" <<< "$projects")
+    local uuid=$(jq -r ".[$i].uuid" <<< "$projects")
+    local name=$(jq -r ".[$i].name" <<< "$projects")
+    project_info=$(curl -s --request GET \
+      --url $COOLIFY_BASE_URL/api/v1/projects/$uuid \
+      --header "$BEARER")
+    local description=$(jq -r ".description" <<< "$project_info")
+    # local environments=$(jq -r ".environments" <<< "$project_info")
+    # echo -e "$environments"
+    # local created_at=$(jq -r ".created_at" <<< "$project_info")
+    # local updated_at=$(jq -r ".updated_at" <<< "$project_info")
+    echo -e "\033[33mProject-$((i+1)):\033[0m \033[1;31m$name\033[0m"
+    echo -e "  Description: $description"
+    echo -e "  ID: $id"
+    echo -e "  UUID: \033[1;37m$uuid\033[0m"
+    # echo -e "  Created At: $created_at"
+    # echo -e "  Updated At: $updated_at"
+  done # End of project loop
+}
 
-list_coolify_servers
+# TEST_UUID="tscs8k4"
+
+# local colors=(black red green yellow blue magenta cyan white reset)
+# local color_codes=(30 31 32 33 34 35 36 37 0)
+
+# local styles=(bold italic underline strikethrough)
+# local style_codes=(1 3 4 9)
+
+# local backgrounds=(black red green yellow blue magenta cyan white)
+# local background_codes=(40 41 42 43 44 45 46 47)
+
+
+
+# Detect if the `jq` command line tool is installed and available
+detect_jq
+
+# Get the UUID of the server to deploy to:
+# get_coolify_servers
+# while true; do
+#   read -p $'\033[33mEnter the UUID of the Server to Deploy to:\033[0m ' COOLIFY_SERVER_UUID
+#   if [ -z "$COOLIFY_SERVER_UUID" ]; then
+#     echo -e "\033[31mPlease enter a valid UUID!\033[0m"
+#   else
+#     get_coolify_servers "$COOLIFY_SERVER_UUID"
+#     if [ $? -eq 0 ]; then
+#       break
+#     fi
+#   fi
+# done
+
+# Now, we need to select the Coolify Project to house the deployments
 while true; do
-  read -p $'\033[33mEnter the UUID of the Server to Deploy to:\033[0m ' COOLIFY_SERVER_UUID
-  if [ -z "$COOLIFY_SERVER_UUID" ]; then
-    echo -e "\033[31mPlease enter a valid UUID!\033[0m"
-  else
-    list_coolify_servers "$COOLIFY_SERVER_UUID"
-    if [ $? -eq 0 ]; then
-      break
-    fi
+  read -p $'\033[33mDeploy to New Project or Add to existing one? \033[31mDefault: New \033[33m[New/List/Add]:\033[0m ' new_list_or_add
+  if [ -z "$new_list_or_add" ]; then
+    new_list_or_add="NEW"
+  fi
+  new_list_or_add=$(echo "$new_list_or_add" | tr '[:lower:]' '[:upper:]')
+  if [ "$new_list_or_add" == "NEW" ]; then
+    echo -e "NEW"
+    break
+  elif [ "$new_list_or_add" == "LIST" ]; then
+    list_coolify_projects
+  elif [ "$new_list_or_add" == "ADD" ]; then
+    echo -e "ADD"
+    break
   fi
 done
 
 
+
+# Display the data we got from Coolify
 COOLIFY_VERSION=$(get_coolify_version)
+echo
 echo -e "\033[33mCoolify API Key:\033[0m $COOLIFY_API_KEY"
 echo -e "\033[33mCoolify Version:\033[0m $COOLIFY_VERSION"
 echo -e "\033[33mDeploying to Server:\033[0m $COOLIFY_SERVER_UUID\033[0m"
+
+COOLIFY_PROJECT_UUID=$(get_coolify_project_uuid)
+COOLIFY_ENVIRONMENT_NAME="production"
+COOLIFY_GITHUB_APP_UUID=""
+COOLIFY_GIT_REPO=""
+COOLIFY_GIT_BRANCH="main"
+COOLIFY_CLIENT_PORTS_EXPOSES="80"
+COOLIFY_SERVER_PORTS_EXPOSES="$PORT"
+COOLIFY_CLIENT_BUILDPACK="nixpacks"
+COOLIFY_SERVER_BUILDPACK="dockerfile"
+COOLIFY_CLIENT_APP_NAME="My Cool Fucking Frontent"
+COOLIFY_SERVER_APP_NAME="My Cool Fucking Backend"
+COOLIFY_CLIENT_DESCRIPTION="This is a cool fucking frontend"
+COOLIFY_SERVER_DESCRIPTION="This is a cool fucking backend"
+COOLIFY_CLIENT_IS_STATIC="true"
+COOLIFY_CLIENT_BASE_DIR="/"
+COOLIFY_SERVER_BASE_DIR="/deploy/server"
+COOLIFY_CLIENT_PUBLISH_DIR="/deploy/client"
+COOLIFY_SERVER_DOCKERFILE="/Dockerfile"
+COOLIFY_CLIENT_INSTANT_DEPLOY="true"
+COOLIFY_SERVER_INSTANT_DEPLOY="true"
